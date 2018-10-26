93d9bc1d38c3c22c276065fb6bbd052726b309f3
iver56 <ij@aiascience.com>
2018-10-26 15:16:20 +0200

    Reduce number of parameters in TCN, so it becomes approximately the same as the LSTM

--- tcn_model.py
+++ tcn_model.py
@@ -6,7 +6,7 @@ from tcn import TCN
 
 def get_tcn_model(input_vector_size, target_vector_size):
     model_input = Input(shape=(None, input_vector_size))
-    model_output = TCN(return_sequences=True)(model_input)
+    model_output = TCN(return_sequences=True, nb_filters=48)(model_input)
     model_output = Dense(target_vector_size, activation="relu")(model_output)
 
     model = Model(inputs=[model_input], outputs=[model_output])
